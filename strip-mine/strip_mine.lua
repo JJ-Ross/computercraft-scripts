ecc88407c065f374015aceb7acd104c6e1e12e9b
JJ-Ross <jeffreysrossjr@gmail.com>
2022-08-21 18:19:04 -0400

    strip-mine

--- strip-mine/strip_mine.lua
+++ strip-mine/strip_mine.lua
@@ -1,9 +1,28 @@
 function dropInventory()
-    for i = 1,9,1 do 
+    for i = 1,16,1 do 
         turtle.select(i)
         turtle.drop()
     end
 end
+
+function ifNotItems(itemList)
+    for j = 1,16,1 do
+        turtle.select(j)
+        isItem = false;
+        for i = 0,table.getn(itemList),1
+            do
+                if (turtle.getItemDetail().name == itemList[i]) do
+                    isItem = true;
+                end
+            end
+        end
+        if (isItem = false) do
+        turtle.drop()
+        end
+    end
+end
+
+itemList = {"minecraft:raw_copper", "minecraft:diamond", "minecraft:redstone", "minecraft:raw_iron", "minecraft:raw_gold", "minecraft:lapis_lazuli", "minecraft:coal"}
 
 function digFour()
     turtle.dig()
