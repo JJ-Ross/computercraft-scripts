c5555d66ac300a602d26e75e9fceb4f32d28b5e2
Zeb <zebmperkins@gmail.com>
2022-08-22 01:30:30 -0400

    update dig

--- landscaper/dig.lua
+++ landscaper/dig.lua
@@ -10,7 +10,7 @@ for x = 1, len do
             turtle.digDown()
             if z ~= hgt - 1 then turtle.down() end
         end
-        for i = 1, hgt - 1 do turtle.up() end
+        for i = 1, hgt - 2 do turtle.up() end
         if y ~= wid then
             turtle.dig()
             turtle.forward()
