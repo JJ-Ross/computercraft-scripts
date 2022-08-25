dc6cbc6e3517bcb055c9eec2845b348fcf80c441
Zeb <zebmperkins@gmail.com>
2022-08-24 21:08:14 -0400

    update farm

--- farmer/farm.lua
+++ farmer/farm.lua
@@ -1,7 +1,7 @@
 farmed = 0;
 
 function dropInventory()
-    for i = 1,16,1 do 
+    for i = 2,16,1 do 
         turtle.select(i)
         turtle.drop()
     end
@@ -9,13 +9,16 @@ end
 
 function isReady()
     p1, p2 = turtle.inspect()
-    if (p2.state.age = 7) then
+    if (p2.state.age == 7) then
         return true
+    else
+        return false
     end
+        
 end
 
 function farm()
-    if (isReady) then
+        if (isReady()) then
         turtle.dig()
         turtle.place()
         turtle.turnRight()
@@ -26,12 +29,15 @@ function farm()
 end
 
 while true do
+    sleep(3)
     farm()
-    if farmed = 8 then
+    if farmed == 8 then
+        turtle.turnLeft()
         for i = 1,8,1 do
             turtle.forward()
         end
         dropInventory()
         turtle.turnRight()
+        farmed = 0
     end
 end
