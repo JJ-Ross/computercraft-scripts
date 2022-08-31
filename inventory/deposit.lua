edd117fed2db2bb10a325d16d3087aac1c04a7e7
Zeb <zebmperkins@gmail.com>
2022-08-31 00:03:01 -0400

    update db

--- inventory/deposit.lua
+++ inventory/deposit.lua
@@ -1,10 +1,11 @@
 args = { ... }
 
-modem = peripheral.find("modem") or error("Server can't start without modem", 0)
+modem = peripheral.wrap("right") or error("Server can't start without modem", 0)
 port = tonumber(args[1]) or 20
+endpoint = tonumber(args[2]) or 21
 modem.open(port)
 print("listening on port "..port.."...")
 
-modem.transmit(21, 20, "mongo select test minecraft:dirt")
+modem.transmit(endpoint, port, "mongo help")
 event, side, channel, replyChannel, message, distance = os.pullEvent("modem_message")
 print(message)
